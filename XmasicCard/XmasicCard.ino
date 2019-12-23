aab0be0ec5d74dc065961f17b84945a4cae79816
Charlie Jaewoong Mun <40027494+happybono@users.noreply.github.com>
2019-12-23 09:20:43 +0900

    Update XmasicCard.ino

--- XmasicCard/XmasicCard.ino
+++ XmasicCard/XmasicCard.ino
@@ -8,51 +8,53 @@
 
 #include "pitches.h"
 #define PIEZO 5
-
+    
 /* 징글벨 음계 : 알기 쉽게 다장조의 음계로 변환하여 처리. */
-int jingleBell_Notes [ ] = { NOTE_G4, NOTE_E5, NOTE_D5, NOTE_C5, NOTE_G4, 0, // 솔미레도솔(쉼표)
-NOTE_G4, NOTE_E5, NOTE_D5, NOTE_C5, NOTE_A4, 0,                              // 솔미레도라(쉼표)
-NOTE_A4, NOTE_F5, NOTE_E5, NOTE_D5, NOTE_B4, 0,                              // 라파미레시(쉼표)
-NOTE_G5, NOTE_G5, NOTE_F5, NOTE_D5, NOTE_E5, NOTE_C5, 0,                     // 솔솔파레미도(쉼표)
-NOTE_G4, NOTE_E5, NOTE_D5, NOTE_C5, NOTE_G4, 0,                              // 솔미레도솔(쉼표)
-NOTE_G4, NOTE_E5, NOTE_D5, NOTE_C5, NOTE_A4, 0,                              // 솔미레도라(쉼표)
-NOTE_A4, NOTE_F5, NOTE_E5, NOTE_D5,                                          // 라파미레
-NOTE_G4, NOTE_G4, NOTE_G4, NOTE_G4,                                          // 솔솔솔솔
-NOTE_A4, NOTE_G4, NOTE_F5, NOTE_D5, NOTE_C5, NOTE_G4, 0,                     // 라솔파레도솔(쉼표)
-NOTE_E5, NOTE_E5, NOTE_E5, NOTE_E5, NOTE_E5, NOTE_E5,                        // 미미미미미미
-NOTE_E5, NOTE_G4, NOTE_C5, NOTE_D5, NOTE_E5, 0,                              // 미솔도레미(쉼표)
-NOTE_F5, NOTE_F5, NOTE_F5, NOTE_F5,                                          // 파파파파
-NOTE_F5, NOTE_E5, NOTE_E5, NOTE_E5,                                          // 파미미미
-NOTE_E5, NOTE_D5, NOTE_D5, NOTE_C5, NOTE_D5, NOTE_G4, 0,                     // 미레레도레솔(쉼표)
-NOTE_E5, NOTE_E5, NOTE_E5, NOTE_E5, NOTE_E5, NOTE_E5,                        // 미미미미미미
-NOTE_E5, NOTE_G4, NOTE_C5, NOTE_D5, NOTE_E5, 0,                              // 미솔도레미(쉼표)
-NOTE_F5, NOTE_F5, NOTE_F5, NOTE_F5,                                          // 파파파파
-NOTE_F5, NOTE_E5, NOTE_E5, NOTE_E5,                                          // 파미미미
-NOTE_G4, NOTE_G4, NOTE_F5, NOTE_D5, NOTE_C5, 0 };                            // 솔솔파레도(쉼표) 
+int jinglebell_Notes[ ] = { NOTE_G4, NOTE_E5, NOTE_D5, NOTE_C5, NOTE_G4, 0,       // 솔미레도솔(쉼표)
+                 NOTE_G4, NOTE_E5, NOTE_D5, NOTE_C5, NOTE_A4, 0,                  // 솔미레도라(쉼표)
+                 NOTE_A4, NOTE_F5, NOTE_E5, NOTE_D5, NOTE_B4, 0,                  // 라파미레시(쉼표)
+                 NOTE_G5, NOTE_G5, NOTE_F5, NOTE_D5, NOTE_E5, NOTE_C5, 0,         // 솔솔파레미도(쉼표)
+                 NOTE_G4, NOTE_E5, NOTE_D5, NOTE_C5, NOTE_G4, 0,                  // 솔미레도솔(쉼표)
+                 NOTE_G4, NOTE_E5, NOTE_D5, NOTE_C5, NOTE_A4, 0,                  // 솔미레도라(쉼표)
+                 NOTE_A4, NOTE_F5, NOTE_E5, NOTE_D5,                              // 라파미레
+                 NOTE_G4, NOTE_G4, NOTE_G4, NOTE_G4,                              // 솔솔솔솔
+                 NOTE_A4, NOTE_G4, NOTE_F5, NOTE_D5, NOTE_C5, NOTE_G4, 0,         // 라솔파레도솔(쉼표)
+                 NOTE_E5, NOTE_E5, NOTE_E5, NOTE_E5, NOTE_E5, NOTE_E5,            // 미미미미미미
+                 NOTE_E5, NOTE_G4, NOTE_C5, NOTE_D5, NOTE_E5, 0,                  // 미솔도레미(쉼표)
+                 NOTE_F5, NOTE_F5, NOTE_F5, NOTE_F5,                              // 파파파파
+                 NOTE_F5, NOTE_E5, NOTE_E5, NOTE_E5,                              // 파미미미
+                 NOTE_E5, NOTE_D5, NOTE_D5, NOTE_C5, NOTE_D5, NOTE_G4, 0,         // 미레레도레솔(쉼표)
+                 NOTE_E5, NOTE_E5, NOTE_E5, NOTE_E5, NOTE_E5, NOTE_E5,            // 미미미미미미
+                 NOTE_E5, NOTE_G4, NOTE_C5, NOTE_D5, NOTE_E5, 0,                  // 미솔도레미(쉼표)
+                 NOTE_F5, NOTE_F5, NOTE_F5, NOTE_F5,                              // 파파파파
+                 NOTE_F5, NOTE_E5, NOTE_E5, NOTE_E5,                              // 파미미미
+                 NOTE_G4, NOTE_G4, NOTE_F5, NOTE_D5, NOTE_C5, 0                   // 솔솔파레도(쉼표)
+               };
 
 /* 징글벨 소절 연주 시간 */
-int jingleBell_Rhythm [ ] = { 250, 250, 250, 250, 750, 250,                    // 8 분 음표 (quaver) = 250 ms (milliseconds)
-250, 250, 250, 250, 750, 250,
-250, 250, 250, 250, 750, 250,
-250, 250, 250, 250, 500, 250, 250,
-250, 250, 250, 250, 750, 250,
-250, 250, 250, 250, 750, 250,
-250, 250, 250, 250,
-750, 250, 250, 250,
-250, 250, 250, 250, 500, 250, 250,
-250, 250, 250, 250, 250, 250, 
-250, 250, 750, 250, 750, 250,
-250, 250, 750, 250,
-250, 250, 250, 250,
-250, 250, 250, 250, 250, 250, 250,
-250, 250, 250, 250, 250, 250,
-250, 250, 750, 250, 750, 250,
-250, 250, 750, 250,
-250, 250, 250, 250,
-250, 250, 250, 250, 750, 250 };
+int jinglebell_Rhythm[ ] = { 250, 250, 250, 250, 750, 250,                        // 8 분 음표 (quaver) = 250 ms (milliseconds)
+                250, 250, 250, 250, 750, 250,
+                250, 250, 250, 250, 750, 250,
+                250, 250, 250, 250, 500, 250, 250,
+                250, 250, 250, 250, 750, 250,
+                250, 250, 250, 250, 750, 250,
+                250, 250, 250, 250,
+                750, 250, 250, 250,
+                250, 250, 250, 250, 500, 250, 250,
+                250, 250, 250, 250, 250, 250,
+                250, 250, 750, 250, 750, 250,
+                250, 250, 750, 250,
+                250, 250, 250, 250,
+                250, 250, 250, 250, 500, 250, 250,
+                250, 250, 250, 250, 250, 250,
+                250, 250, 750, 250, 750, 250,
+                250, 250, 750, 250,
+                250, 250, 250, 250,
+                250, 250, 250, 250, 750, 250
+              };
 
-/* Kristen Bell - Do you want to build a snowman 음계 */
-int snowman_Notes [ ] = {NOTE_F4, NOTE_AS4, NOTE_DS5, NOTE_G4, NOTE_AS4, NOTE_DS5, NOTE_GS4, NOTE_AS4,
+/* Do You Want to Build a Snowman 음계 */
+int snowman_Notes [] = {NOTE_F4, NOTE_AS4, NOTE_DS5, NOTE_G4, NOTE_AS4, NOTE_DS5, NOTE_GS4, NOTE_AS4,
                         NOTE_F4, NOTE_AS4, NOTE_DS5, NOTE_G4, NOTE_AS4, NOTE_DS5, NOTE_GS4, NOTE_AS4,
                         NOTE_F4, NOTE_AS4, NOTE_DS5, NOTE_G4, NOTE_AS4, NOTE_DS5, NOTE_GS4, NOTE_AS4,
                         NOTE_F4, NOTE_AS4, NOTE_DS5, NOTE_G4, NOTE_AS4, NOTE_DS5, NOTE_GS4, NOTE_AS4, 0,
@@ -67,8 +69,9 @@ int snowman_Notes [ ] = {NOTE_F4, NOTE_AS4, NOTE_DS5, NOTE_G4, NOTE_AS4, NOTE_DS
                         NOTE_DS4, NOTE_D4, NOTE_DS3
                        };
 
-/* Kristen Bell - Do you want to build a snowman 소절 연주 시간 */
-int snowman_Rhythm [ ] = {250, 250, 250, 250, 250, 250, 250, 250,
+
+/* Do You Want to Build a Snowman 소절 연주 시간 */
+int snowman_Rhythm [] = {250, 250, 250, 250, 250, 250, 250, 250,
                          250, 250, 250, 250, 250, 250, 250, 250,
                          250, 250, 250, 250, 250, 250, 250, 250,
                          250, 250, 250, 250, 250, 250, 250, 250, 250,
@@ -84,18 +87,36 @@ int snowman_Rhythm [ ] = {250, 250, 250, 250, 250, 250, 250, 250,
                         };
 
 
-void setup()                                                                 // 한 번만 연주하게 하기 위하여,
-                                                                             // 실행 프로그램을 setup() 에서 처리.
+void setup()                                                                      // 한 번만 연주하게 하기 위하여,
+                                                                                  // 실행 프로그램을 setup() 에서 처리.
 {
-int i;
-pinMode(PIEZO, OUTPUT);                                                      // PIEZO 버저핀은 출력 모드로 설정.
-delay(3000);                                                                 // 카드를 열고 3 초 후에 연주 시작. 
-for (i=0; i<105; i++)                                                        // 연주 길이만큼 숫자 조정.
-{
-tone(PIEZO, jingleBell_Notes[i], jingleBell_Rhythm[i]);                      // 배열 순으로 노래 재생
-delay(time[i]);
+  int randNumber;
+  int i;
+  
+  randomSeed(analogRead(0));                                                      // 연결되지 않은 핀에서 임의의 노이즈 (신호) 발생.
+  randNumber = random(2);                                                         // 난수 발생 
+  Serial.println(randNumber);
+  pinMode(PIEZO, OUTPUT);                                                         // PIEZO 버저핀은 출력 모드로 설정.
+  delay(3000);                                                                    // 카드를 열고 3 초 후에 연주 시작.
+
+  if (randNumber == 0)                                                            // 발생된 난수 값이 0 인 경우
+  {
+    for (i = 0; i < 105; i++)                                                     // 연주 길이만큼 숫자 조정.
+    {
+      tone(PIEZO, jinglebell_Notes[i], jinglebell_Rhythm[i]);                     // 배열 순으로 노래 재생
+      delay(jinglebell_Rhythm[i]);
+    }
+  }
+  else if (randNumber == 1)                                                       // 발생된 난수 값이 1 인 경우
+  {
+    for (i = 0; i < 116; i++)                                                     // 연주 길이만큼 숫자 조정.
+    {
+      tone(PIEZO, snowman_Notes[i], snowman_Rhythm[i]);                           // 배열 순으로 노래 재생
+      delay(snowman_Rhythm[i]);
+    }
+  }
 }
-}
-void loop()                                                                  // 한 번만 연주하게 하기 위하여
-{                                                                            // loop()에서는 아무 것도 처리하지 않음
+void loop()                                                                       // 한 번만 연주하게 하기 위하여
+{                                                                                 // loop()에서는 아무 것도 처리하지 않음
+
 }
